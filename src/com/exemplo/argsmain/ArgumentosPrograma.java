c343643e7fb00a0275997f312eea66e4a26d679c
William Rafael <analista.wrnunes@gmail.com>
2022-06-29 23:34:58 -0300

    alteracao de wesley para wesleyTeste

--- src/com/exemplo/argsmain/ArgumentosPrograma.java
+++ src/com/exemplo/argsmain/ArgumentosPrograma.java
@@ -4,13 +4,13 @@ public class ArgumentosPrograma {
 
 	public static void main(String[] args) {
 		
-		args[0] = "Wesley";
+		args[0] = "Wesley teste";
 		args[1] = "DEv Java";
 		args[2] = "007";
 		
-		System.out.println("O primeiro elemento é: " + args[0]);
-		System.out.println("O segundo elemento é: " + args[1]);
-		System.out.println("O terceiro elemento é: " + args[2]);
+		System.out.println("O primeiro elemento ï¿½: " + args[0]);
+		System.out.println("O segundo elemento ï¿½: " + args[1]);
+		System.out.println("O terceiro elemento ï¿½: " + args[2]);
 		
 
 	}
